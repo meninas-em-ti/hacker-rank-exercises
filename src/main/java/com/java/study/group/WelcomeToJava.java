4556bc4330bd8ef1b448550c12715dcb38e1017f
Joyce <joyce.apsmartins@gmail.com>
2021-11-03 21:40:54 +0000

    Welcome to Java!

---
+++ src/main/java/com/java/study/group/WelcomeToJava.java
@@ -0,0 +1,10 @@
+package com.java.study.group;
+
+public class WelcomeToJava
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello, World." );
+        System.out.println( "Hello, Java." );
+    }
+}
